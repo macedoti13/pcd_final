b517572eac1b1875d7ae987ef66622cee7d8869b
macedoti13 <thialmacedo@gmail.com>
2024-06-20 17:29:07 -0300

    bunch of stuff

--- app/run.py
+++ app/run.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 import pandas as pd
-import pickle
 import sys
 import os
 
@@ -17,3 +16,79 @@ from functions.plots.question_4_and_7_plots import create_questions_4_and_7_plot
 from functions.plots.question_5_plots import create_question_5_plot
 from functions.plots.question_6_plots import generate_question_6_plots
 
+# load datasets
+water_consumption_silver = pd.read_parquet(os.path.join(os.path.dirname(__file__), "../data/silver/water_consumption_silver.parquet"))
+question_2_dataset = pd.read_parquet(os.path.join(os.path.dirname(__file__), "../data/gold/question_2_answer.parquet"))
+question_3_dataset = pd.read_parquet(os.path.join(os.path.dirname(__file__), "../data/gold/question_3_answer.parquet"))
+
+# app 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/static-plots')
+def static_plots():
+    
+    # question 2 plots
+    question_2_plot_1 = generate_question_2_plot_1(question_2_dataset)
+    question_2_plot_2 = generate_question_2_plot_2(question_2_dataset)
+    
+    # question 3 plots
+    question_3_plot_1 = generate_question_3_plot_1(question_3_dataset)
+    question_3_plot_2 = generate_question_3_plot_2(question_3_dataset)
+    
+    # question 6 plots
+    question_6_plot_1, question_6_plot_2, question_6_plot_3 = generate_question_6_plots(water_consumption_silver)
+    
+    # render template
+    return render_template(
+        'static_plots.html',
+        plot_html=question_2_plot_1,
+        plot_2_html=question_2_plot_2,
+        plot_3_html=question_3_plot_1,
+        plot_4_html=question_3_plot_2,
+        plot_5_html=question_6_plot_1,
+        plot_6_html=question_6_plot_2,
+        plot_7_html=question_6_plot_3
+    )
+
+@app.route('/forecasting-plots', methods=['GET', 'POST'])
+def forecasting_plots():
+    
+    # wait user input
+    if request.method == "POST":
+        year = request.form['year']
+        month = request.form['month']
+        day = request.form['day']
+        hour = request.form['hour']
+        
+        # generate the data from the user input
+        output_flow_prediction_dataset = forecast_next_24_hours_output_flow_rate(year, month, day, hour, save_df=False)
+        simulation_dataset = simulate_empyting_reservoir(year, month, day, hour, return_df=True)
+        
+        # forecast the next 24 hours output flow rate and simulate the emptying of the reservoir
+        question_4_and_7_plot_1 = create_questions_4_and_7_plot_1(output_flow_prediction_dataset)
+        question_5_plot = create_question_5_plot(simulation_dataset)
+        
+        # render template with the plots
+        return render_template(
+            'forecasting_plots.html',
+            plot_html=question_4_and_7_plot_1,
+            plot_2_html=question_5_plot
+        )
+        
+    # render the template with the form to get the user input
+    return render_template('forecasting_plots.html')
+        
+
+@app.route('/model-performance')
+def model_performance():
+    return render_template('model_performance.html')
+
+def main():
+    app.run(host='0.0.0.0', port=3000, debug=True)
+    
+if __name__ == "__main__":
+    main() 
--- functions/answer_questions/answer_question_5.py
+++ functions/answer_questions/answer_question_5.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '')))
 from answer_question_4_and_7 import forecast_next_24_hours_output_flow_rate
 import pandas as pd
 
-def simulate_empyting_reservoir(year, month, day, hour):
+def simulate_empyting_reservoir(year, month, day, hour, return_df=True):
     original_input_df = pd.read_parquet(os.path.join(os.path.dirname(__file__),"../../data/silver/water_consumption_silver.parquet")) 
     input_df = original_input_df[original_input_df["timestamp"] == pd.Timestamp(year=year, month=month, day=day, hour=hour)]
     start_index = input_df.index.values.tolist()[0]
@@ -23,5 +23,7 @@ def simulate_empyting_reservoir(year, month, day, hour):
     concated_df.loc[:, 'simulation'] = True
     yesterday_df.loc[:, 'simulation'] = False
     concated_df = pd.concat([yesterday_df[['timestamp', 'reservoir_level_percentage', 'output_flow_rate', 'simulation']], concated_df], axis=0).reset_index(drop=True)
+    if return_df: 
+        return concated_df
     concated_df.to_parquet(os.path.join(os.path.dirname(__file__),"../../data/gold/question_5_answer.parquet"))
     return hours_until_empyting
